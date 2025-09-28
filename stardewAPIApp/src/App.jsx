c16f43868cd7fa4da9dbcfd8fc542de0f3c273bd
Simon Gao <simongao398@gmail.com>
2025-09-28 15:26:46 -0400

    added initial html of inputSection and useState variable that holds user options data

--- stardewAPIApp/src/App.jsx
+++ stardewAPIApp/src/App.jsx
@@ -21,17 +21,33 @@ export default function App () {
     .then (data => {
       // obtain response.json as data
       setCropData(data); // set the crop data to data
+      console.log(data);
     })
     .catch (error => {
       console.log("Error: " + error); // catch and log any errors
     })
   }, []);
-  console.log(cropData); // testing purposes
 
 
-  const [userOptions, setUserOptions] = useState({}); // holds all the data the user selected in InputSection 
+  const [userOptions, setUserOptions] = useState({
+    // default states:
+    season: "spring",
+    farmingLevel: 1,
+    sprinklers: {
+      normal: 0,
+      quality: 0,
+      iridium: 0
+    },
+    tillerProf: false,
+    artisanProf: false,
+    kegs: 0,
+    preservesJars: 0
+  }); // holds all the data the user selected in InputSection 
+  
   const [calendarSquares, setCalendarSquares] = useState([]); // holds all the squares (28 in total) for the days in the month (each square is an object with data)
   const [currentOutputData, setOutputData] = useState({}); // holds all relevent output data to be displayed in output section (altered throughout calendar section)
+  
+  console.log(userOptions); // TESTING INPUT SELECTION
   return (
     <>
       <InputSection userOptions={userOptions} setUserOptions={setUserOptions}/>
