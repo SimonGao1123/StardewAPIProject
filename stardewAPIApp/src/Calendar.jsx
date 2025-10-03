aa671caae25c32aaab354b620f6d448de795275a
Simon Gao <simongao398@gmail.com>
2025-10-03 10:16:16 -0400

    altered newPlantAdd to contain crop/fertilizer objects

--- stardewAPIApp/src/Calendar.jsx
+++ stardewAPIApp/src/Calendar.jsx
@@ -12,14 +12,14 @@ function CalendarDayOverlay () {
 }
 
 function AddCropPopUp ({dayNumber, season, cropData, calendarSquares, setCalendarSquares, fertilizerData}) {
-    const [cropSelected, setCropSelected] = useState("");
+    const [cropSelected, setCropSelected] = useState(0); // CROP ID, makes searching cropData much faster
     const [numberOfCrops, setNumCrops] = useState(1);
-    const [fertSelected, setFertSelected] = useState("none"); // auto chooses first fertilizer
+    const [fertSelected, setFertSelected] = useState(0); // FERTILIZER ID, auto chooses no fertilizer (if select value == 0 then none was chosen)
 
     const fertilizerOptions = [];
     for (const fertilizer of fertilizerData) {
         fertilizerOptions.push(
-            <option key={fertilizer.name} value={fertilizer.name}>{nameNormalizer(fertilizer.name)}</option>
+            <option key={fertilizer.name} value={fertilizer.id}>{nameNormalizer(fertilizer.name)}</option>
         );
     }
     return (
@@ -29,26 +29,39 @@ function AddCropPopUp ({dayNumber, season, cropData, calendarSquares, setCalenda
             <label htmlFor="number_of_crops">Number of Crops</label>
             <input id="number_of_crops" type="number" min="1" step="1" value={numberOfCrops} onChange={(e) => setNumCrops(parseInt(e.target.value))}/> 
 
-            <select id="fertilizer-select" value={fertSelected} onChange={(e) => setFertSelected(e.target.value)}>
-                <option key={"null"} value={"none"}>None</option>
+            <select id="fertilizer-select" value={fertSelected} onChange={(e) => setFertSelected(parseInt(e.target.value))}>
+                <option key={"null"} value={0}>None</option>
                 {fertilizerOptions}
             </select>
 
-            <button id="add-crop-btn" onClick ={() => addCropCalendarSquare(dayNumber, cropSelected, numberOfCrops, fertSelected, null, calendarSquares, setCalendarSquares)}>Add Crop</button>
+            <button id="add-crop-btn" onClick ={() => addCropCalendarSquare(
+                dayNumber, 
+                cropData[cropSelected-1], 
+                numberOfCrops, 
+                fertSelected ? fertilizerData[fertSelected-1] : null, 
+                null, 
+                calendarSquares, 
+                setCalendarSquares)}>Add Crop</button>
         </div>
     </>
 
     // IMPLEMENT METHOD OF SELLING LATER
     );
 }
-const addCropCalendarSquare = (dayNumber, cropName, numberOfCrops, fertilizerType, prepType, calendarSquares, setCalendarSquares) => {
+const addCropCalendarSquare = (dayNumber, crop, numberOfCrops, fertilizerType, prepType, calendarSquares, setCalendarSquares) => {
     // WILL ADD PREP TIME LATER
     const newPlantAdd = {
-        name: cropName,
+        crop: crop,
         dayPlanted: dayNumber,
-        number: numberOfCrops,
+        numberPlanted: numberOfCrops,
         fertilizer: fertilizerType
     }; // data for new plant added (planting)
+    /*
+    crop = object, from cropData 
+    dayPlanted = integer (day crop was initially planted)
+    numberPlanted = integer (# of crops planted)
+    fertilizer = object, from fertilizerData
+    */  
 
     
     console.log (newPlantAdd); // TESTING, add calendar square representing day 1 
@@ -62,17 +75,19 @@ function SelectCropType ({season, cropData, setCropSelected, cropSelected}) {
     
     useEffect(() => {
         if (!cropSelected && possiblePlants.length > 0) {
-            setCropSelected(possiblePlants[0].name);
+            setCropSelected(possiblePlants[0].id);
         }
     }, [cropSelected, possiblePlants, setCropSelected]);
     // will ONLY run if the crop selected changes or the the array of possible plants changes
 
+    
     const listOfPossible = [];
     for (const plant of possiblePlants) {
-        listOfPossible.push(<option value={plant.name} key={plant.name}>{nameNormalizer(plant.name)}</option>);
+        listOfPossible.push(<option value={plant.id} key={plant.name}>{nameNormalizer(plant.name)}</option>);
+        // HOLDS THE PLANT ID AS IT'S VALUE, 
     }
     return (
-        <select id="crop_selection" onChange={(e) => setCropSelected(e.target.value)} value={cropSelected}>
+        <select id="crop_selection" onChange={(e) => setCropSelected(parseInt(e.target.value))} value={cropSelected}>
             {listOfPossible}
         </select>
     )
